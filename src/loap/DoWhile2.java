f7de07600c6184663c977ba7200636b4b0d5f78e
jiin <jyl08840@naver.com>
2024-11-30 18:40:34 +0900

    do-while문2

---
+++ src/loap/DoWhile2.java
@@ -0,0 +1,14 @@
+package loap;
+
+public class DoWhile2 {
+
+    public static void main(String[] args){
+        int i = 10;
+        do {
+            System.out.println("현재 숫자는: " + i);
+            i++;
+           }while (i < 3);
+        }
+        //참이면 do를 실행 거짓이면 while문을 빠져나간다.
+    }
+
