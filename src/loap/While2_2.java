f6cfd6e95816a1bff4021d0bc246d6c2a3ddcfd9
jiin <jyl08840@naver.com>
2024-11-30 17:28:38 +0900

    while문2

---
+++ src/loap/While2_2.java
@@ -0,0 +1,25 @@
+package loap;
+
+public class While2_2 {
+    //반복문 시작
+    public static void main(String[] args) {
+        int sum =0;
+        //sum = sum + 1;
+        int i = 1;
+
+        sum = sum + i;
+        System.out.println("i=" + i + " sum=" + sum);
+        i++;
+
+        sum = sum + i;
+        System.out.println("i=" + i + " sum=" + sum);
+        i++;
+
+
+        sum = sum + i;
+        System.out.println("i=" + i + " sum=" + sum);
+
+
+    }
+
+}
