5c657369f9bcabb781c806f99d19025c91926bb5
jiin <jyl08840@naver.com>
2024-12-01 14:34:38 +0900

    Continue

---
+++ src/loap/Continue1.java
@@ -0,0 +1,22 @@
+package loap;
+
+public class Continue1 {
+
+    public static void main(String[] args){
+        int sum = 0;
+         int i=1;
+
+
+         //i=1,2,3,4,5
+         while(i <= 5){
+           if( i ==3 ){
+               i++;
+               continue;
+
+             }
+             System.out.println(i);
+             i++;
+         }
+        }
+    }
+
