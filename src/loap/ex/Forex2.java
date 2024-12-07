e5088c086e1c504ebb16a852db69cba89ac95301
jiin <jyl08840@naver.com>
2024-12-07 16:54:16 +0900

    짝수 출력 - for

--- src/loap/ex/Forex2.java
+++ src/loap/ex/Forex2.java
@@ -1,7 +1,8 @@
 package loap.ex;
 
-public class Forex2  {
 
+public class Forex2  {
+   //짝수 출력
     public static void main(String[] args){
         int num = 2;
         for(int count = 1; count <=10; count++) {
