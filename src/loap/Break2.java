f991ad978ce934fb5378807f203fcbdb2c9503a4
jiin <jyl08840@naver.com>
2024-12-07 16:46:27 +0900

    자연수 출력 - while

---
+++ src/loap/Break2.java
@@ -0,0 +1,20 @@
+package loap;
+
+public class Break2 {
+
+    public static void main(String[] args){
+        int sum = 0;
+
+
+        for(int i=1;  ; i++ ){
+             sum = sum + i;//sum=1+2+3+4+5
+             if( sum > 10){
+                 System.out.println("합이 10보다 크면 종료: i=" + i + " sum=" + sum);
+                 break;
+
+             }
+
+           }
+        }
+    }
+
